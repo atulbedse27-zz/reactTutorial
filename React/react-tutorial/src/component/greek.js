4ae2e7bf5b069e391c9e06a2c31c34153f366815
atulbedse27 <atulbedse27@gmail.com>
2020-07-25 12:23:10 +0530

    We have learned about functional component.

---
+++ React/react-tutorial/src/component/greek.js
@@ -0,0 +1,12 @@
+//FUNCTIONAL COMPONENT
+import React from 'react';
+
+// function Greek () {
+//   return <h1>hello atul</h1> 
+// }
+
+const Greek = () => {return <h1>hello atul</h1>}
+//export const Greek = () => {return <h1>hello atul</h1>}
+// Naming export where we will export but we have to take same naming while importing in app.js
+export default Greek
+//this is the default export we can export here and import with any other name
