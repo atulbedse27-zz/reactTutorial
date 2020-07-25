67424506933d07633fbb9f1f43a5ee30a7584f7d
atulbedse27 <atulbedse27@gmail.com>
2020-07-25 11:37:32 +0530

    we have learned folder structure for react.

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -8,7 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <strong>Package.json:</strong> This file contains dependecies required for project. <br/>
+          <strong>Node_modules:</strong> All the dependecies are installed here.<br/>
+          <strong>Public:</strong> This folder will contain three files<br/>
+                  manifest.json: will concern of progressive webapps.<br/>
+          <strong>favicon.icon:</strong> icon.<br/>
+          <strong>index.hml:</strong> we are making single page application every time view gets dynamically changes but every time this fie will get served up.
+          we will not make any changes in this file. we can make changes in head tag but definatly not in body tag.react will control the UI. for that purpose we have one DIV tag with id="root".<br/>
+
+          <strong>SRC:</strong>         <br/>
         </p>
         <a
           className="App-link"
